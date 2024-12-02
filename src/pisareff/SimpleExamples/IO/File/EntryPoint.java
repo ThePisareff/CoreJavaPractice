97c8bbf5905ac31b1677c8dd411d4976d34f7e9a
grigo <grigorjevskijav@yandex.ru>
2024-12-02 21:28:39 +0300

    Introduction in IO Streams

--- src/pisareff/SimpleExamples/IO/File/EntryPoint.java
+++ src/pisareff/SimpleExamples/IO/File/EntryPoint.java
@@ -1,10 +1,40 @@
 package pisareff.SimpleExamples.IO.File;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class EntryPoint {
     public static void main(String[] args) throws IOException {
+
+    }
+
+    private static void ReadGoingToTheRiverByteByByte() throws IOException {
+        File file = Path.of("resources","GoingToTheRiver.txt").toFile();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] bytes = new byte[inputStream.available()];
+            int currentByteNumber = 0;
+            byte currentByte;
+            while ((currentByte = (byte) inputStream.read()) != -1){
+                bytes[currentByteNumber++] = currentByte;
+            }
+            String stringValue = new String(bytes);
+            System.out.println(stringValue);
+            }
+    }
+
+    private static void ReadGoingToTheRiver() throws IOException {
+        //File file = new File(String.join(File.separator,"resources","file.txt")); // File.separator - разделитель директорий основанный на запускающей системе, для решения данной проблемы есть метод у класса Path.of()
+        File file = Path.of("resources","GoingToTheRiver.txt").toFile();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] bytes = inputStream.readAllBytes();
+            String stringValue = new String(bytes);
+            System.out.println(stringValue);
+        }
+    }
+
+    public void BasicDemo() throws IOException {
         File file = new File("resources/file.txt");
         file.createNewFile();
         System.out.println(file.exists());
